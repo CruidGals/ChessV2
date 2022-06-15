aa1f42c227fd2569b8536908d52daed1c1a788ec
CruidGals <kchiem30@gmail.com>
2022-06-15 14:43:57 -0400

    Finish drag piece function

--- src/Board.java
+++ src/Board.java
@@ -11,6 +11,7 @@ public class Board extends JFrame {
 
     // Vars for dragging
     private static Piece selectedPiece;
+    private static Square selectedPieceParent;
 
     public Board() {
         setSize(816, 839);
@@ -73,34 +74,77 @@ public class Board extends JFrame {
 
             if (e.getButton() == MouseEvent.BUTTON1) {
                 selectedPiece = null;
+                
 
-                if (c instanceof Square)
+                if (c instanceof Square) {
+                    //System.out.println(((Square) c).getPiece());
+                    return;
+                }
+
+                if(((Piece) c).getRank() == Piece.NO_PIECE)
                     return;
 
                 selectedPiece = (Piece) c;
+                selectedPieceParent = (Square) ((Piece) c).getParent();
+                selectedPiece.setSize(selectedPiece.getPreferredSize());
 
                 layeredPanel.add(selectedPiece, JLayeredPane.DRAG_LAYER);
+                
                 selectedPiece.setLocation(e.getX() - 50, e.getY() - 50);
             }
 
             //DEBUG CODE
             if(e.getButton() == MouseEvent.BUTTON3) {
+                if( ((Square) c.getParent()).getComponents().length > 1) {
+                    return;
+                }
+
                 if(c instanceof Piece)
                     c = c.getParent();
+                    System.out.println(c);
 
                 String tempRank = JOptionPane.showInputDialog(null, "Enter rank of this piece. (0 = None, 1 = King, 2 = Pawn, 3 = Bishop, 4 = Knight, 5 = Rook, 6 = Queen)");
+                if(tempRank == null) return;
                 String tempColor = JOptionPane.showInputDialog(null, "Enter color of this piece. (8 = White, 16 = Black) ");
 
                 if(tempRank == null || tempColor == null) return;
 
-                ((Square) c).getPiece().setPiece(Integer.parseInt(tempColor), Integer.parseInt(tempRank));
+                ((Square) c).remove(0);
+                ((Square) c).add(new Piece(Integer.parseInt(tempColor), Integer.parseInt(tempRank)));
+                ((Square) c).validate();
             }
         }
 
         @Override
         public void mouseReleased(MouseEvent e) {
-            Component c = boardPanel.findComponentAt(e.getX(), e.getY());
-            System.out.println(c);
+            if(selectedPiece == null) return;
+
+            selectedPiece.setVisible(false);
+            layeredPanel.remove(selectedPiece);
+            selectedPiece.setVisible(true);
+
+            int xMax = boardPanel.getWidth() - selectedPiece.getWidth();
+            int x = Math.min(e.getX(), xMax);
+            x = Math.max(x, 0);
+    
+            int yMax = boardPanel.getHeight() - selectedPiece.getHeight();
+            int y = Math.min(e.getY(), yMax);
+            y = Math.max(y, 0);
+            
+            
+            Component c = boardPanel.findComponentAt(x, y);
+            if(c != selectedPieceParent) 
+                c = c.getParent();
+            
+            Square targetSquare = (Square) c;
+
+            selectedPieceParent.add(new Piece(Piece.WHITE, Piece.NO_PIECE));
+            selectedPieceParent.validate();
+
+            targetSquare.remove(0);
+            targetSquare.add(selectedPiece);
+            targetSquare.validate();
+
         }
 
     }
--- src/Square.java
+++ src/Square.java
@@ -1,27 +1,22 @@
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
 public class Square extends JLabel {
     public static final Color LIGHT_SQUARE_COLOR = new Color(255,255,255);
     public static final Color DARK_SQUARE_COLOR = new Color(118,150,86);
 
-    private Piece piece;
-
     public Square(Color squareColor, int color, int rank) {
         setBackground(squareColor);
         setOpaque(true);
         setPreferredSize(new Dimension(100,100));
 
-        piece = new Piece(color, rank);
         setLayout(new GridBagLayout());
-        add(piece);
+        add(new Piece(color, rank));
 
     }
 
     public Piece getPiece() {
-        return piece;
+        return (Piece) getComponent(0);
     }
 
-    
 }
